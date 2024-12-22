b5909e7d1da1f6b15334ba0b78d7230e26c8768e
iam-i-j-k <irfanjankhan7860@gmail.com>
2024-12-22 13:07:03 +0530

    3 commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import BarGraph from './BarGraph'
 const Home = () => {
   return (
     <div>
-      <div className="grid grid-cols-2 gap-2 sm:grid-cols-1">
+      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2">
         <div>
           <PieChart />
         </div>
