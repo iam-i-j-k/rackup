3ae2fe58763d712c5eda98d95de5100205083ec3
iam-i-j-k <irfanjankhan7860@gmail.com>
2024-12-22 17:48:23 +0530

    6 commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -128,7 +128,7 @@ const Navbar = () => {
                                 Settings
                                 </a>
                                 <a
-                                href="/user"
+                                href="/"
                                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 >
                                 Sign out
